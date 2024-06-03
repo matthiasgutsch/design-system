700548881e67a2928c3dbc32dc1404b446cfa541
Matthias Gutsch <MGutsch@datacolor.com>
2024-06-03 17:42:09 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Card } from './card/card.component';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Card
+    Card,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/message/message.component.ts
@@ -0,0 +1,82 @@
+import { CommonModule } from '@angular/common';
+import {
+    AfterContentInit,
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  ElementRef,
+  HostBinding,
+  Input,
+  NgModule,
+  TemplateRef,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'dc-message',
+  template: `
+  
+    <div class="dc-message"  [class]="color"  [attr.data-pc-name]="'message'">
+      <div class="dc-message-header" *ngIf="header">
+        <ng-container *ngTemplateOutlet="header"></ng-container>
+      </div>
+        <div class="dc-message-content">
+        
+        <div class="dc-message-header" *ngIf="cardTitle">
+          {{ cardTitle }}
+        </div>
+        <ng-content></ng-content>
+
+        <ng-container *ngTemplateOutlet="content"></ng-container>
+
+      </div>
+      <div class="dc-message-footer" *ngIf="footer">
+        <ng-container *ngTemplateOutlet="footer"></ng-container>
+      </div>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./message.css'],
+  host: {
+    class: 'dc-element',
+  },
+})
+export class MessageComponent implements AfterContentInit {
+    @Input() cardTitle: string | undefined;
+    @Input() class: string | undefined;
+    @Input() content: TemplateRef<any> | null = null;
+    @Input() color: string | undefined;
+
+    @ContentChild('Header')  headerFacet: TemplateRef<any> | any;
+    @ContentChild('Footer')  footerFacet: TemplateRef<any> | any;
+  
+    @ContentChild('header')  header!: TemplateRef<any>;
+    @ContentChild('footer')  footer!: TemplateRef<any>;
+    @ContentChild('content') titleTemplate!: TemplateRef<any>;
+
+
+    @HostBinding('class.dcui-message--color-success') public get isColorSuccess() {
+      return this.color === 'success';
+    }
+  
+    @HostBinding('class.dcui-message--color-alert') public get isColorAlert() {
+      return this.color === 'alert';
+    }
+
+    
+    
+  constructor(private el: ElementRef) {}
+
+  ngAfterContentInit() {
+
+
+  }
+
+ 
+}
+
+@NgModule({
+  imports: [CommonModule],
+})
+export class MessageModule {}
